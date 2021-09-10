4d07c076e480447591eb95478ca98a4210d51e86
ppmasa8 <kiyokiyopip56@gmail.com>
2021-09-10 23:46:29 +0900

    Add shift method

--- ruby_array_test.go
+++ ruby_array_test.go
@@ -4,10 +4,21 @@ import "testing"
 
 func Test_pop(t *testing.T) {
 	var arr intArray = intArray{1, 2, 3, 4, 5}
-	exp := intArray{1, 2, 3, 4}
+	expect := intArray{1, 2, 3, 4}
 	arr.pop()
 	for i, _ := range arr {
-		if arr[i] != exp[i] {
+		if arr[i] != expect[i] {
+			t.Errorf("result=%v, index=%v", arr, i)
+		}
+	}
+}
+
+func Test_shift(t *testing.T) {
+	var arr intArray = intArray{1, 2, 3, 4, 5}
+	expect := intArray{2, 3, 4, 5}
+	arr.shift()
+	for i, _ := range arr {
+		if arr[i] != expect[i] {
 			t.Errorf("result=%v, index=%v", arr, i)
 		}
 	}
