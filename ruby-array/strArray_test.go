a6b486cea067402caedb48b55826e11380ba06be
ppmasa8 <kiyokiyopip56@gmail.com>
2021-11-17 19:26:05 +0900

    Refact for strArray

--- ruby-array/strArray_test.go
+++ ruby-array/strArray_test.go
@@ -5,7 +5,7 @@ import "testing"
 /* string */
 func Test_str_pop(t *testing.T) {
 	// Normal System
-	var arr strArray = strArray{"sunday", "monday", "tuesday", "wednesday", "thursday"}
+	var arr = strArray{"sunday", "monday", "tuesday", "wednesday", "thursday"}
 	expect := strArray{"sunday", "monday", "tuesday", "wednesday"}
 	arr.pop()
 	for i, _ := range arr {
@@ -37,7 +37,7 @@ func Test_str_pop(t *testing.T) {
 
 func Test_str_shift(t *testing.T) {
 	// Normal System
-	var arr strArray = strArray{"sunday", "monday", "tuesday", "wednesday", "thursday"}
+	var arr = strArray{"sunday", "monday", "tuesday", "wednesday", "thursday"}
 	expect := strArray{"monday", "tuesday", "wednesday", "thursday"}
 	arr.shift()
 	for i, _ := range arr {
@@ -69,7 +69,7 @@ func Test_str_shift(t *testing.T) {
 
 func Test_str_push(t *testing.T) {
 	// Normal System
-	var arr strArray = strArray{"sunday", "monday", "tuesday", "wednesday", "thursday"}
+	var arr = strArray{"sunday", "monday", "tuesday", "wednesday", "thursday"}
 	expect := strArray{"sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"}
 	arr.push("friday", "saturday")
 	for i, _ := range arr {
@@ -101,7 +101,7 @@ func Test_str_push(t *testing.T) {
 
 func Test_str_unshift(t *testing.T) {
 	// Normal System
-	var arr strArray = strArray{"sunday", "monday", "tuesday", "wednesday", "thursday"}
+	var arr = strArray{"sunday", "monday", "tuesday", "wednesday", "thursday"}
 	expect := strArray{"thursday", "friday", "saturday", "sunday", "monday", "tuesday", "wednesday", "thursday"}
 	arr.unshift("thursday", "friday", "saturday")
 	for i, _ := range arr {
@@ -133,7 +133,7 @@ func Test_str_unshift(t *testing.T) {
 
 func Test_str_delete(t *testing.T) {
 	// Normal System
-	var arr strArray = strArray{"sunday", "monday", "tuesday", "wednesday", "sunday"}
+	var arr = strArray{"sunday", "monday", "tuesday", "wednesday", "sunday"}
 	expect := strArray{"monday", "tuesday", "wednesday"}
 	arr.delete("sunday")
 	for i, _ := range arr {
@@ -165,7 +165,7 @@ func Test_str_delete(t *testing.T) {
 
 func Test_str_uniq(t *testing.T) {
 	// Normal System
-	var arr strArray = strArray{"sunday", "monday", "tuesday", "wednesday", "sunday"}
+	var arr = strArray{"sunday", "monday", "tuesday", "wednesday", "sunday"}
 	expect := strArray{"sunday", "monday", "tuesday", "wednesday"}
 	arr.uniq()
 	for i, _ := range arr {
@@ -197,7 +197,7 @@ func Test_str_uniq(t *testing.T) {
 
 func Test_str_size(t *testing.T) {
 	// Normal System
-	var arr strArray = strArray{"sunday", "monday", "tuesday", "wednesday", "sunday"}
+	var arr = strArray{"sunday", "monday", "tuesday", "wednesday", "sunday"}
 	expect := 5
 	size := arr.size()
 	if size != expect {
@@ -205,7 +205,7 @@ func Test_str_size(t *testing.T) {
 	}
 
 	// Abnormal System
-	var arr_blank strArray = strArray{}
+	var arr_blank = strArray{}
 	expect_zero := 0
 	blank_size := arr_blank.size()
 	if blank_size != expect_zero {
@@ -215,14 +215,14 @@ func Test_str_size(t *testing.T) {
 
 func Test_str_include(t *testing.T) {
 	// Normal System
-	var arr1 strArray = strArray{"sunday", "monday", "tuesday", "wednesday", "sunday"}
+	var arr1 = strArray{"sunday", "monday", "tuesday", "wednesday", "sunday"}
 	expect1 := true
 	include1 := arr1.include("sunday")
 	if include1 != expect1 {
 		t.Errorf("result=%v, expect=%v", include1, expect1)
 	}
 
-	var arr2 strArray = strArray{"sunday", "monday", "tuesday", "wednesday", "sunday"}
+	var arr2 = strArray{"sunday", "monday", "tuesday", "wednesday", "sunday"}
 	expect2 := false
 	include2 := arr2.include("saturday")
 	if include2 != expect2 {
@@ -230,7 +230,7 @@ func Test_str_include(t *testing.T) {
 	}
 
 	// Abnormal System
-	var arr_blank strArray = strArray{}
+	var arr_blank = strArray{}
 	expect_blank := false
 	blank_include := arr_blank.include("saturday")
 	if blank_include != expect_blank {
@@ -240,7 +240,7 @@ func Test_str_include(t *testing.T) {
 
 func Test_str_first(t *testing.T) {
 	// Normal System
-	var arr strArray = strArray{"sunday", "monday", "tuesday", "wednesday", "sunday"}
+	var arr = strArray{"sunday", "monday", "tuesday", "wednesday", "sunday"}
 	expect := "sunday"
 	first := arr.first()
 	if first != expect {
@@ -248,7 +248,7 @@ func Test_str_first(t *testing.T) {
 	}
 
 	// Abnormal System
-	var arr_blank strArray = strArray{}
+	var arr_blank = strArray{}
 	expect_blank := "nil"
 	blank_first := arr_blank.first()
 	if blank_first != expect_blank {
@@ -258,7 +258,7 @@ func Test_str_first(t *testing.T) {
 
 func Test_str_last(t *testing.T) {
 	// Normal System
-	var arr strArray = strArray{"sunday", "monday", "tuesday", "wednesday", "sunday"}
+	var arr = strArray{"sunday", "monday", "tuesday", "wednesday", "sunday"}
 	expect := "sunday"
 	last := arr.last()
 	if last != expect {
@@ -266,7 +266,7 @@ func Test_str_last(t *testing.T) {
 	}
 
 	// Abnormal System
-	var arr_blank strArray = strArray{}
+	var arr_blank = strArray{}
 	expect_blank := "nil"
 	blank_last := arr_blank.last()
 	if blank_last != expect_blank {
@@ -276,7 +276,7 @@ func Test_str_last(t *testing.T) {
 
 func Test_str_combination(t *testing.T) {
 	// Normal Systems
-	var arr strArray = strArray{"sunday", "monday", "tuesday", "wednesday", "thursday"}
+	var arr = strArray{"sunday", "monday", "tuesday", "wednesday", "thursday"}
 	expect := [][]string{{"sunday", "monday"}, {"sunday", "tuesday"}, {"monday", "tuesday"},
 		{"sunday", "wednesday"}, {"monday", "wednesday"}, {"tuesday", "wednesday"}, {"sunday", "thursday"},
 		{"monday", "thursday"}, {"tuesday", "thursday"}, {"wednesday", "thursday"}}
@@ -290,8 +290,8 @@ func Test_str_combination(t *testing.T) {
 	}
 
 	// Abnormal Systems
-	var over_arr strArray = strArray{"sunday", "monday", "tuesday", "wednesday", "thursday"}
-	var over_expect strArray = strArray{}
+	var over_arr = strArray{"sunday", "monday", "tuesday", "wednesday", "thursday"}
+	var over_expect = strArray{}
 	over_combination := over_arr.combination(6)
 	if len(over_combination) != len(over_expect) {
 		t.Errorf("result=%v, expect=%v", over_combination, over_expect)
@@ -299,7 +299,7 @@ func Test_str_combination(t *testing.T) {
 }
 
 func Test_str_reverse(t *testing.T) {
-	var arr strArray = strArray{"sunday", "monday", "tuesday", "wednesday", "thursday"}
+	var arr = strArray{"sunday", "monday", "tuesday", "wednesday", "thursday"}
 	expect := strArray{"thursday", "wednesday", "tuesday", "monday", "sunday"}
 	rev := arr.reverse()
 	for i, v := range rev {
@@ -310,19 +310,19 @@ func Test_str_reverse(t *testing.T) {
 }
 
 func Test_str_empty(t *testing.T) {
-	var arr strArray = strArray{"sunday", "monday", "tuesday", "wednesday", "thursday"}
+	var arr = strArray{"sunday", "monday", "tuesday", "wednesday", "thursday"}
 	if arr.empty() != false {
 		t.Errorf("result=%v, expect=%v", arr.empty(), false)
 	}
 
-	var blank_arr strArray = strArray{}
+	var blank_arr = strArray{}
 	if blank_arr.empty() != true {
 		t.Errorf("result=%v, expect=%v", blank_arr.empty(), true)
 	}
 }
 
 func Test_str_sort(t *testing.T) {
-	var arr strArray = strArray{"sunday", "monday", "tuesday", "wednesday", "thursday"}
+	var arr = strArray{"sunday", "monday", "tuesday", "wednesday", "thursday"}
 	expect := strArray{"monday", "sunday", "thursday", "tuesday", "wednesday"}
 	arr.sort()
 	for i, _ := range arr {
